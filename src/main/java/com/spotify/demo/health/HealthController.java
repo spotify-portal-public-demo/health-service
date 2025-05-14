fc4a0a63e03175cf040fb5a853ea71b9f2e696fa
Paul Frybarger <pfrybarger@spotify.com>
2025-05-14 11:10:41 +0100

    allow changing health value

--- src/main/java/com/spotify/demo/health/HealthController.java
+++ src/main/java/com/spotify/demo/health/HealthController.java
@@ -3,6 +3,7 @@ package com.spotify.demo.health;
 import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/health")
 public class HealthController {
 
+  private volatile boolean alerting = true;
+
   @GetMapping("/{service}")
   public Map<String, Health> service(@PathVariable String service) {
-    return Map.of("main", new Health(true, true, 0), "staging", new Health(true, true, 2));
+    return Map.of("main", new Health(alerting, true, 0), "staging", new Health(alerting, true, 2));
   }
 
+  @PostMapping("/toggle")
+  public void toggle() {
+    alerting = !alerting;
+  }
 }
